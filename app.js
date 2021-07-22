cb618dc4c604e7a546d242c6255e213556d092e0
Navian <primeguyy@gmail.com>
2021-07-22 12:01:49 +0000

    writing tests for functions in app.js

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const isAnagram = (str1, str2) => {
   return neutralizeStr(str1) === neutralizeStr(str2);
 };
 
-const neutralizeStr = () => {
-  return str.replace(/^\w/g, "").toLowerCase().split("").sort().join("");
+const neutralizeStr = (str) => {
+  return str.replace(/[^\w]/g, "").toLowerCase().split("").sort().join("");
 };
 
 module.exports = {
--- app.test.js
+++ app.test.js
@@ -1,1 +1,32 @@
+const { isAnagram, neutralizeStr } = require("./app");
 
+// check if both functions are defined or not
+test("isAnagram function is defined", () => {
+  expect(isAnagram).toBeDefined();
+});
+
+test("neutralizeStr function is defined", () => {
+  expect(typeof neutralizeStr).toBe("function");
+});
+
+// check if neutralizeStr is able to parse strings into the expected format
+test("'dragon' should be 'adgnor' when passed into neutralizeStr", () => {
+  expect(neutralizeStr("dragon")).toBe("adgnor");
+});
+
+test("neutralizeStr should be able to handle whitespace in strings", () => {
+  expect(neutralizeStr("hello there boy")).toMatch(/\w/g);
+});
+
+// check if isAnagram is able to validate two given strings as anagrams
+test("'reed' is an anagram of 'deer'", () => {
+  const result = isAnagram("reed", "deer");
+
+  expect(result).toBeTruthy();
+});
+
+test("'Dormitory' is an anagram of 'dirty room'", () => {
+  const result = isAnagram("Dormitory", "dirty room");
+
+  expect(result).toBeTruthy();
+});
