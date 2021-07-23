38b8f2be69c00315a5b81d8d60b28de0c90ed580
Navian <primeguyy@gmail.com>
2021-07-23 20:37:06 +0000

    updated form styles and added validation buttons to each form

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 import { isAnagram } from "./anagram.js";
 import { isPalindrome } from "./palindrome.js";
 
-console.log(isAnagram("door", "rodo"));
-console.log(isPalindrome("redivider"));
+const choiceSelect = document.getElementById("choiceSelect");
+const palindromeInput = document.getElementById("palindromeInput");
+const firstAnagramInput = document.getElementById("firstAnagramInput");
+const secondAnagramInput = document.getElementById("secondAnagramInput");
+
+// console.log(isAnagram("door", "rodo"));
+// console.log(isPalindrome("redivider"));
