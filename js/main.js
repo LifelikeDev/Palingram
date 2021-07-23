71898755973488fdb15574def026ba93d6132ce9
Navian <primeguyy@gmail.com>
2021-07-23 21:51:03 +0000

    forms input field validation added

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ import { isAnagram } from "./anagram.js";
 import { isPalindrome } from "./palindrome.js";
 
 const choiceSelect = document.getElementById("choiceSelect");
+const options = document.querySelectorAll("#choiceSelect option");
 const palindromeOption = document.querySelector(".palindromeOption");
 const anagramOption = document.querySelector(".anagramOption");
 const palindromeForm = document.getElementById("palindromeForm");
@@ -10,26 +11,49 @@ const palindromeInput = document.getElementById("palindromeInput");
 const firstAnagramInput = document.getElementById("firstAnagramInput");
 const secondAnagramInput = document.getElementById("secondAnagramInput");
 
+window.addEventListener("load", () => {
+  for (let i = 0; i < options.length; i++) {
+    options[i].selected = options[i].defaultSelected;
+  }
+});
+
+// displaying input fields when user selects a choice
 choiceSelect.addEventListener("change", () => {
   const selectedChoice = choiceSelect.selectedOptions[0].text;
 
   if (selectedChoice === "Palindrome") {
-    palindromeOption.style.display = "block";
+    if (anagramOption.style.display === "block") {
+      anagramOption.style.display = "none";
+      palindromeOption.style.display = "block";
+    } else {
+      palindromeOption.style.display = "block";
+    }
+  } else if (selectedChoice === "Anagram") {
+    if (palindromeOption.style.display === "block") {
+      palindromeOption.style.display = "none";
+      anagramOption.style.display = "block";
+    } else {
+      anagramOption.style.display = "block";
+    }
+  } else {
+    palindromeOption.style.display = "none";
     anagramOption.style.display = "none";
-  }
-  if (selectedChoice === "Anagram") {
-    anagramOption.style.display = "block";
-    palindromeOption.style.display = "block";
   }
 
   console.log(selectedChoice);
 });
 
+// handling palindrome form
 palindromeForm.addEventListener("submit", () => {
-  console.log(palindromeInput.value);
-  palindromeInput.value = "";
+  if (palindromeInput.value === "") {
+    alert("You need to enter a word or phrase");
+  } else {
+    console.log(palindromeInput.value);
+    palindromeInput.value = "";
+  }
 });
 
+// handling anagram form
 anagramForm.addEventListener("submit", () => {
   if (firstAnagramInput.value !== "" && secondAnagramInput.value !== "") {
     console.log(firstAnagramInput.value, secondAnagramInput.value);
